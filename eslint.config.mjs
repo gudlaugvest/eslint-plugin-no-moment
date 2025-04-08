f6fe83f127608c665032499f687cebe485e7fbf2
gudlaugvest <112634914+gudlaugvest@users.noreply.github.com>
2025-04-08 20:20:40 +0000

    fixed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,31 @@
-import { defineConfig } from "eslint";
 import globals from "globals";
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-import noMoment from "eslint-plugin-no-moment";
+import noMomentPlugin from "./index.js";
 
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  noMoment.configs.recommended,
-]);
+export default [
+  {
+    ignores: ["node_modules/**"],
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    }
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+        ...globals.browser
+      },
+    },
+    plugins: {
+      noMoment: noMomentPlugin
+    },
+    rules: {
+      'noMoment/no-moment': 'error'
+    }
+  }
+];
